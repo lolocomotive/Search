687dd09a8371b2e00935126b5e84b18660771584
lolocomotive <lolocomotive181@gmail.com>
2020-12-31 12:31:07 +0100

    Getting to a point where it starts to work

--- indexer.php
+++ indexer.php
@@ -1,74 +1,66 @@
 <?php
 include 'dbconnect.php';
+session_start();
+const MAX_QUERY_SIZE = 8192;
+const START_SQL = "INSERT INTO files_tmp (Name,Path) VALUES ";
+$fp = 0;
+$dbConn = OpenCon();
+$arr = [START_SQL];
+$start_time = microtime(true);
 
-function indexDir($path = '.', $level = 0, $dbConn, $arr, $start_sql, $fp)
+function indexDir($path = '.')
 {
-    $ignore = array('.', '..', '$RECYCLE.BIN');
-    // Directories to ignore when listing output. Many hosts 
-    // will deny PHP access to the cgi-bin. 
-
-    $dh = @opendir($path);
-    // Open the directory to the handle $dh 
-
+    global $arr, $fp;
+    $ignore = array('.', '..', '$RECYCLE.BIN'); // Filenames to be ignored
+    $dh = @opendir($path); // Directory handle
     while (false !== ($file = readdir($dh))) {
-        // Loop through the directory 
-
         if (!in_array($file, $ignore)) {
-            // Check that this file is not to be ignored 
-
-            $spaces = str_repeat('&nbsp;', ($level * 4));
-            // Just to add spacing to the list, to better 
-            // show the directory tree. 
-
             if (is_dir("$path/$file")) {
-                // Its a directory, so we need to keep reading down... 
-                indexDir("$path\\$file", ($level + 1), $dbConn, $arr, $start_sql, $fp++);
-                // Re-call this same function but on a new directory. 
-                // this is what makes function recursive. 
-
+                indexDir("$path\\$file");
             } else {
                 $sql = "('" . $file . "','" . str_replace("\\", "\\\\", $path) . "\\\\" . $file . "')";
-                $arr = bulkSQL($arr, 128, $start_sql, $sql, $dbConn);
+                $arr = bulkSQL($arr, $sql);
                 $fp++;
             }
         }
     }
-
     closedir($dh);
-    // Close the directory handle 
 }
 function toString($arr)
 {
-    $r = "";
+    $r = ""; // Return value
     foreach ($arr as $str) {
         $r .= $str;
     }
     return $r;
 }
-function bulkSQL($arr, $max_arr, $start_sql, $sql, $dbConn)
+function bulkSQL($arr, $sql)
 {
-    if (count($arr) == $max_arr) {
+    global $dbConn, $start_time, $fp;
+    if (count($arr) == MAX_QUERY_SIZE) {
         $query = toString($arr) . $sql . ";";
         mysqli_query($dbConn, $query);
-        return [$start_sql];
+        $len = microtime(true) - $start_time;
+        $_SESSION["elapsed"] = $len;
+        $_SESSION["fp"] = $fp;
+        return [START_SQL];
     } else {
         array_push($arr, $sql . ",");
         return $arr;
     }
 }
-function SQLFlush($arr, $dbConn)
+function SQLFlush($arr)
 {
-    $query = toString($arr) . ";";
+    global $dbConn;
+    $query = substr(toString($arr), 0, -1) . ";";
     mysqli_query($dbConn, $query);
+    echo mysqli_error($dbConn);
 }
-
-$files_processed = 0;
-$dbConn = OpenCon();
-$start_sql = "INSERT INTO files_tmp (Name,Path) VALUES ";
-$arr = [$start_sql];
-var_dump($arr);
-indexDir("D:\\loic\\Pokered", 0, $dbConn, $arr, $start_sql, $files_processed);
-echo "done";
-var_dump($files_processed);
-SQLFlush($arr, $dbConn);
+$sql = "TRUNCATE TABLE  files_tmp;";
+mysqli_query($dbConn, $sql);
+set_time_limit(0);
+indexDir("D:");
+SQLFlush($arr);
 closeCon($dbConn);
+$len = microtime(true) - $start_time;
+echo "Done !<br/> " . $fp . " files in " . $len . " seconds (" . $fp / $len . " files per second)";
